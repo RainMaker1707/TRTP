6633edac9dc167a2100f45b81f54cd319c510272
Rain Maker <hadrien.allegaert@student.uclouvain.be>
2022-03-13 16:44:19 +0100

    add send and receive data

---
+++ src/addresser.c
@@ -0,0 +1,43 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <netdb.h>
+
+#include <sys/time.h>
+#include <sys/select.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+
+#include <arpa/inet.h>
+
+#include <poll.h>
+
+#include <netinet/in.h>
+#include <unistd.h>
+#include <errno.h>
+
+const char * real_address(const char *address, struct sockaddr_in6 *rval){
+    // protocol setup
+    struct addrinfo head;
+    head.ai_flags = 0;
+    head.ai_family =  AF_INET6;
+    head.ai_socktype = SOCK_DGRAM;
+    head.ai_protocol = IPPROTO_UDP;
+    // result waiting for init
+    struct addrinfo *result = NULL;
+    // setup result
+    int s = getaddrinfo(address, NULL, &head, &result);
+    if (s != 0) {
+        const char *error = gai_strerror(s);
+        return error;
+    }
+    // setup return pointer to ipv6 addr struct
+    struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *) result->ai_addr;
+    *rval = *ipv6;
+    freeaddrinfo(result);
+    return NULL;
+}
---
+++ src/wait.c
@@ -0,0 +1,46 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <netdb.h>
+
+#include <sys/time.h>
+#include <sys/select.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+
+#include <arpa/inet.h>
+
+#include <poll.h>
+
+#include <netinet/in.h>
+#include <unistd.h>
+#include <errno.h>
+
+int wait_for_client(int sfd){
+    char *buffer = (char*) malloc(sizeof(char*) * 528);
+    if(!buffer) return -1;
+    socklen_t addr_len = sizeof(struct sockaddr_in6);
+    struct sockaddr_in6 *src_addr = (struct sockaddr_in6 *)malloc(sizeof(struct sockaddr_in6));
+    // receive error
+    if(recvfrom(sfd, (void *)buffer, 528, MSG_PEEK, (struct sockaddr*) src_addr,&addr_len )<0){
+        //garbage
+        free(buffer);
+        free(src_addr);
+        return -1;
+    }
+    // connection error
+    if(connect(sfd,(struct sockaddr*) src_addr,addr_len)<0){
+        //garbage
+        free(buffer);
+        free(src_addr);
+        return -1;
+    }
+    // garbage
+    free(buffer);
+    free(src_addr);
+    return 0;
+}

c1263a3ffeef09337fdb7489be0f4b59ea27bbab
Rain Maker <hadrien.allegaert@student.uclouvain.be>
2022-03-22 14:31:16 +0100

    sender seems to be ok

--- src/sender.c
+++ src/sender.c
@@ -3,14 +3,158 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+#include <poll.h>
+#include <string.h>
 
 #include "log.h"
+#include "queue.h"
+#include "real_address.h"
+#include "create_socket.h"
+#include "packet_interface.h"
+
+
+uint32_t timestamp;
+bool file_red = false;
+queue_t* queue;
+uint8_t last_ack = -1;
+//TODO statistics
 
 int print_usage(char *prog_name) {
     ERROR("Usage:\n\t%s [-f filename] [-s stats_filename] [-c] receiver_ip receiver_port", prog_name);
     return EXIT_FAILURE;
 }
 
+uint32_t get_timestamp(){
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    return (uint32_t) (now.tv_sec*1000 + now.tv_usec/1000);;
+}
+
+bool pkt_send(int sock, pkt_t* pkt){
+    /// SEND packet and UPDATE timestamp
+    size_t len = (size_t) 16 + pkt_get_length(pkt);
+    char buff[len];
+    pkt_set_timestamp(pkt, get_timestamp()); // UPDATE TIMESTAMP ON LAUNCH
+    if(pkt_encode(pkt, buff, &len) != PKT_OK) return false; // ERROR ON PACKET ENCODING
+    ssize_t error = write(sock, buff, len);  // SEND PACKET
+    if(error < 0) return false;
+    return true;
+}
+
+bool checker(uint8_t seq, int window_size){
+    /// Check if seqnum is in sequence
+    if(seq == last_ack) return false;
+    if(seq > last_ack || seq <= (last_ack + window_size)%256) return true;
+    return false;
+}
+
+bool ack_nack_dispatch(int sock){
+    pkt_t* pkt = pkt_new();
+    char buff[10];
+    ssize_t len = read(sock, buff, 10);
+    if(len < 0){
+        pkt_del(pkt);
+        return false;
+    }
+    if(pkt_decode(buff, len, pkt) != PKT_OK){
+        pkt_del(pkt);
+        return false;
+    }
+    if(pkt_get_tr(pkt)){
+        pkt_del(pkt);
+        return false;
+    }
+    if(pkt_get_type(pkt) != PTYPE_DATA && pkt_get_type(pkt) != PTYPE_NACK){
+        pkt_del(pkt);
+        return false;
+    }
+    queue->maxSize = pkt_get_window(pkt);
+    uint8_t seq_num = pkt_get_seqnum(pkt);
+    timestamp = pkt_get_timestamp(pkt);
+    /// Free useless stored packet in queue
+    if(pkt_get_type(pkt) == PTYPE_ACK && checker(seq_num, MAX_WINDOW_SIZE)){
+        while(seq_num < pkt_get_seqnum(queue->head->pkt)) {
+            free(queue_pop(queue));
+            seq_num += 0; // ONLY TO AVOID WARNING NOT UPDATED
+        }
+        last_ack = pkt_get_seqnum(pkt);
+    }if(pkt_get_type(pkt) == PTYPE_NACK){
+        /// RESEND PACKET WITH NACK
+        if(!checker((seq_num + 1) % 256, queue->maxSize)){
+            pkt_del(pkt);
+            return false;
+        }
+        node_t* current = queue->head;
+        while(pkt_get_seqnum(current->pkt) != seq_num) current = current->next;
+        pkt_send(sock, current->pkt);
+    }
+    pkt_del(pkt);
+    return true;
+}
+
+int sender_agent(int sock, char* filename){
+    bool finished = false;
+    struct pollfd poll_fd[2];
+    poll_fd[0].fd = sock;
+    poll_fd[0].events = POLLIN;
+    char buff[MAX_PAYLOAD_SIZE];
+    FILE* file;
+    uint8_t seq_num = 0;
+    if(filename) file = fopen(filename, "r");
+    fprintf(stderr, "%s\n", filename);
+    while(!finished){
+        /// READ file part by part
+        while(!file_red && queue->size < queue->maxSize){
+            size_t red_len = fread(buff, sizeof(char), sizeof(buff) -1, file); // SEGFAULT
+            fprintf(stderr, "%s\n\n", buff);
+            if(!red_len) file_red = true;
+            else{
+                /// SETUP and SEND packet
+                pkt_t* pkt = pkt_new();
+                pkt_set_type(pkt, PTYPE_DATA); // SETTING TYPE ON DATA
+                pkt_set_tr(pkt, 0); // NOT TRUNCATED
+                pkt_set_window(pkt, 0);  // NO WINDOW, UPDATED FROM ACK or NACK
+                pkt_set_length(pkt, red_len); // LEN GET FROM FREAD()
+                pkt_set_seqnum(pkt, seq_num); // SETTING UP SEQ_NUM
+                pkt_set_timestamp(pkt, 0); // NEED TO BE UPDATED
+                pkt_set_payload(pkt, buff, red_len); // SETTING UP BUFFER TO PAYLOAD
+                seq_num = (seq_num + 1) % 256; // UPDATE SEQNUM 256 because 8 bits count at 255 max.
+                if(pkt_send(sock, pkt)) queue_push_pkt(queue, pkt);
+                memset(buff, 0, red_len);
+            }
+        }
+        /// WAIT for ACK and NACK
+        int poll_fdd = poll(poll_fd, 1, 0); // 0 == no timeout
+        if(poll_fdd > 1) ack_nack_dispatch(sock);
+        /// SEND packet expired
+        node_t* current = queue->head;
+        while(current != NULL){
+            if(get_timestamp() - pkt_get_timestamp(current->pkt) > 4000) pkt_send(sock, current->pkt);
+            current = current->next;
+        }
+        if(file_red && queue->size == 0) finished = true;
+        if(get_timestamp() - timestamp >= 30000) { /// FORCE STOP after 30s
+            while(queue->size > 0) free(queue_pop(queue));
+            finished = true;
+        }
+
+    }
+    /// FINAL SEND
+    pkt_t *pkt = pkt_new();
+    pkt_set_type(pkt, 1);
+    pkt_set_tr(pkt, 0);
+    pkt_set_window(pkt, 0);
+    pkt_set_length(pkt, 0);
+    pkt_set_seqnum(pkt, last_ack);
+    pkt_set_timestamp(pkt,0);
+    pkt_set_payload(pkt,NULL,0);
+    pkt_send(sock, pkt);
+    pkt_del(pkt);
+    if(filename)fclose(file);
+    return 1;
+}
 
 int main(int argc, char **argv) {
     int opt;
@@ -21,7 +165,6 @@ int main(int argc, char **argv) {
     char *receiver_port_err;
     bool fec_enabled = false;
     uint16_t receiver_port;
-
     while ((opt = getopt(argc, argv, "f:s:hc")) != -1) {
         switch (opt) {
         case 'f':
@@ -63,5 +206,15 @@ int main(int argc, char **argv) {
     ERROR("This is not an error, %s", "now let's code!");
 
     // Now let's code!
-    return EXIT_SUCCESS;
+    struct  sockaddr_in6 ip;
+    if(real_address(receiver_ip, &ip)) return  EXIT_FAILURE;
+    int  sock = create_socket(NULL, -1, &ip, receiver_port);
+    if(sock < 0) {
+        fprintf(stderr, "Error on socket creation");
+        return EXIT_FAILURE;
+    }
+    timestamp = get_timestamp();
+    queue = queue_new();
+    setup_queue(queue, 1);
+    return sender_agent(sock, filename);
 }

44557e000e12cf4379bc3bcd39b0a2b3794adbd3
Rain Maker <hadrien.allegaert@student.uclouvain.be>
2022-03-22 14:30:45 +0100

    add queue utils

---
+++ src/queue.c
@@ -0,0 +1,100 @@
+#include "queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+/*
+ * This file is an ADT for a pkt linked list FIFO, using two struct
+ * queue_t: which is a basically a linked_list with size AND a NULL pointer at the end.
+ * node_t: which is a node storing a pkt_t* and its follower node_t*
+ */
+
+queue_t* queue_new(){
+    queue_t* queue = (queue_t*)malloc(sizeof(queue_t));
+    if(!queue) return NULL;
+    queue->size = 0;
+    queue->maxSize = 1;
+    queue->head = NULL;
+    queue->current = NULL;
+    queue->tail = NULL;
+    return queue;
+}
+
+node_t* node_new(){
+    node_t* node = (node_t*)malloc(sizeof(node_t));
+    if(!node) return NULL;
+    node->pkt = NULL;
+    node->next = NULL;
+    return node;
+}
+
+void setup_queue(queue_t* queue, int maxSize){
+    queue->maxSize = maxSize;
+}
+void setup_node(node_t* node, pkt_t* pkt, node_t* next){
+    node->pkt = pkt;
+    node->next = next;
+}
+
+int queue_get_size(queue_t* queue){
+    return queue->size;
+}
+int queue_get_max_size(queue_t* queue){
+    return queue->maxSize;
+}
+
+node_t* queue_get_head(queue_t* queue){
+    return queue->head;
+}
+node_t* queue_get_tail(queue_t* queue){
+    return queue->tail;
+}
+
+node_t* queue_get_current(queue_t* queue){
+    return queue->current;
+}
+
+void queue_push(queue_t* queue, node_t* to_push){
+    if(!queue || !to_push) return;
+    if(queue->size == queue->maxSize) return;
+    if(queue->size == 0){
+        queue->head = to_push;
+        queue->tail = to_push;
+        queue->current = to_push;
+        queue->size = 1;
+
+    }else {
+        queue->tail->next = to_push;
+        queue->tail = to_push;
+        queue->size += 1;
+    }
+}
+
+void queue_push_pkt(queue_t* queue, pkt_t* pkt){
+    node_t* to_push = node_new();
+    setup_node(to_push, pkt, NULL);
+    queue_push(queue, to_push);
+}
+
+node_t* queue_pop(queue_t* queue){
+    if(queue->size == 0) return NULL;
+    node_t* to_return = queue->head;
+    queue->head = queue->head->next;
+    queue->size -= 1;
+    return to_return; // TODO NOT FORGET TO FREE NODE
+}
+int queue_insert_at(queue_t* queue, node_t* to_push, int index);
+node_t* queue_remove_at(queue_t* queue, int index);
+
+/*
+int main(int argc, char* argv[]){
+    queue_t* queue = queue_new();
+    setup_queue(queue, 10);
+    printf("queue size: %d\n", queue_get_size(queue));
+    queue_push(queue, node_new());
+    printf("queue size: %d\n", queue_get_size(queue));
+    queue->current = queue->head;
+    while(queue_pop(queue)) printf("POP\n");
+    return EXIT_SUCCESS;
+}
+ */
---
+++ src/queue.h
@@ -0,0 +1,42 @@
+#ifndef RESEAU_QUEUE_H
+#define RESEAU_QUEUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "packet_interface.h"
+
+typedef struct node{
+    pkt_t *pkt;
+    struct node* next;
+}node_t;
+
+typedef struct queue{
+    ssize_t size;
+    node_t* head;
+    node_t* tail;
+    node_t* current;
+    ssize_t maxSize;
+}queue_t;
+
+queue_t* queue_new();
+node_t* node_new();
+
+void setup_queue(queue_t* queue, int maxSize);
+void setup_node(node_t* node, pkt_t* pkt, node_t* next);
+
+int queue_get_size(queue_t* queue);
+int queue_get_max_size(queue_t* queue);
+
+node_t* queue_get_head(queue_t* queue);
+node_t* queue_get_tail(queue_t* queue);
+node_t* queue_get_current(queue_t* queue);
+
+void queue_push(queue_t* queue, node_t* to_push);
+node_t* queue_pop(queue_t* queue);
+void queue_push_pkt(queue_t* queue, pkt_t* pkt);
+
+int queue_insert_at(queue_t* queue, node_t* to_push, int index); // TODO
+node_t* queue_remove_at(queue_t* queue, int index);  // TODO
+
+
+#endif //RESEAU_QUEUE_H

294ec50b401bbc96814d581d3a705713bd1bc855
Rain Maker <hadrien.allegaert@student.uclouvain.be>
2022-03-25 23:41:43 +0100

    update shell broken test

--- tests/simple_test.sh
+++ tests/simple_test.sh
@@ -14,7 +14,7 @@ if [ ! -z "$VALGRIND" ] ; then
 fi
 
 # On lance le receiver et capture sa sortie standard
-$valgrind ./receiver -f received_file :: 2456  2> receiver.log &
+$valgrind ./new_receiver :: 8088  1>received_file 2>receiver.log
 receiver_pid=$!
 
 cleanup()
@@ -26,7 +26,7 @@ cleanup()
 trap cleanup SIGINT  # Kill les process en arrière plan en cas de ^-C
 
 # On démarre le transfert
-if ! $valgrind ./sender ::1 1341 < input_file 2> sender.log ; then
+if ! $valgrind ./new_sender ::1 8088 < input_file 2>sender.log ; then
   echo "Crash du sender!"
   cat sender.log
   err=1  # On enregistre l'erreur

8071464a71b31d81c00aa89c4073bfb532cdf777
Rain Maker <hadrien.allegaert@student.uclouvain.be>
2022-04-27 17:46:08 +0200

    update benchmark

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy.interpolate
 import numpy as np
 
 if __name__ == "__main__":
@@ -18,11 +19,19 @@ if __name__ == "__main__":
         dic[key] = val[1]/val[0]
         temp_tab.append((key, dic.get(key)))
     temp_tab.sort()
-    x_tab = [elem[0] for elem in temp_tab]
-    y_tab = [elem[1] for elem in temp_tab]
+    x_tab = np.array([elem[0] for elem in temp_tab])
+    y_tab = np.array([elem[1] for elem in temp_tab])
+    print(len(x_tab))
     fig, ax = plt.subplots()
     ax.plot(x_tab, y_tab, linewidth=2.0)
-    ax.set_title("Perfect network transfer simulation")
+    ax.set_title("Perfect network transfer simulation (linear interpolation on red)")
     ax.set_ylabel("Time (s)")
     ax.set_xlabel("Number of bytes")
+    print(x_tab[-1])
+    x_full = np.arange(start=x_tab[0], stop=x_tab[-1], step=(x_tab[-1]-x_tab[0])/5)
+    y_inter = scipy.interpolate.interp1d(x_tab, y_tab)
+    y_inter = [y_inter(x) for x in x_full]
+    ax.plot(x_full, y_inter, linewidth=1.0, color='red')
+    print(len(x_tab), len(y_tab))
     plt.show()
+
